ceefb159a261da42a5a88c746584c74997ddc5ef
Krishna Varshney <commercekv2003@gmail.com>
2024-07-13 12:56:42 +0530

    file add

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  }
+});
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -21,4 +36,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
